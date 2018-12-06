aa11b5497e79e0a9b88cc01880813355ff8e96f8
moonrailgun <moonrailgun@gmail.com>
2018-12-06 14:33:43 +0800

    :star: item 组合元素

--- src/lib/item.js
+++ src/lib/item.js
@@ -1,15 +1,19 @@
 export const createItem = function(view, {x, y}) {
-  const rect = view.append('rect')
-    .attr('x', x)
-    .attr('y', y)
+  const item = view.append('g')
+    .attr('transform', `translate(${x} ${y})`)
+
+  const rect = item.append('rect')
+    .attr('x', 0)
+    .attr('y', 0)
     .attr('rx', 10)
     .attr('ry', 10)
     .attr('width', 100)
     .attr('height', 100)
     .attr('fill', 'red')
 
-  rect.append('circle')
-    .attr('cx', 10)
-    .attr('cy', 10)
+  item.append('circle')
+    .attr('cx', 20)
+    .attr('cy', 20)
     .attr('r', 10)
+    .attr('fill', 'white')
 }
